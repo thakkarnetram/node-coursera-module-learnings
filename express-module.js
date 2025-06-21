266ccd08b011365e95f7151c676924235c0bb5dc
thakkarnetram <thakkarnetram10@gmail.com>
2025-06-21 22:59:48 +0530

    express basics p2

--- express-module.js
+++ express-module.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
 const PORT = 8080 || process.env.PORT
 
 const object = [
@@ -21,8 +22,26 @@ const object = [
     }
 ]
 
+const arr = [3,45,62,34,1]
+
 app.get("/",(req,res) => {
     res.send(object)
+})
+
+app.post("/",(req,res) => {
+    const {name,age,role} = req.body;
+    object.push({name, age, role})
+    return res.send(object)
+})
+
+app.get("/arr",(req,res) => {
+    res.send(arr)
+})
+
+app.post("/arr",(req,res) => {
+    const {number,index} = req.body;
+    arr[index] = number;
+    return res.send(arr)
 })
 
 app.listen(PORT,()=> {
